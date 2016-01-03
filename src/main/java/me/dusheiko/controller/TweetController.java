04911f9a3a59974dbf0c7ceab51c0f97b9f186a6
dudi <dusheyko.dima@gmail.com>
2016-01-03 22:40:52 +0200

    Add search tweets functionality

--- src/main/java/me/dusheiko/controller/TweetController.java
+++ src/main/java/me/dusheiko/controller/TweetController.java
@@ -2,6 +2,8 @@ package me.dusheiko.controller;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.social.twitter.api.SearchResults;
 import org.springframework.social.twitter.api.Tweet;
@@ -9,7 +11,9 @@ import org.springframework.social.twitter.api.Twitter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class TweetController {
@@ -17,6 +21,22 @@ public class TweetController {
 	private Twitter twitter;
 	
 	@RequestMapping("/")
+	public String home() {
+		return "searchPage";
+	}
+	
+	@RequestMapping(value="/postSearch", method=RequestMethod.POST)
+	public String postSearch(HttpServletRequest request, RedirectAttributes redirectAttributes) {
+		String search = request.getParameter("search");
+		if("struts".equals(search)) {
+			redirectAttributes.addFlashAttribute("error", "Try using not " + search);
+			return "redirect:/";
+		}
+		redirectAttributes.addAttribute("search", search);
+		return "redirect:result";
+	}
+	
+	@RequestMapping("/result")
 	public String hello(@RequestParam(defaultValue = "s_vakarchuk") String search, Model model) {
 		SearchResults searchResults = twitter.searchOperations().search(search);
 		List<Tweet> tweets = searchResults.getTweets();
