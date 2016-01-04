a0cc5df4264f7929a219c023a606317bf9aa6446
dudi <dusheyko.dima@gmail.com>
2016-01-04 23:32:15 +0200

    add validations

--- src/main/java/me/dusheiko/profile/ProfileController.java
+++ src/main/java/me/dusheiko/profile/ProfileController.java
@@ -2,7 +2,10 @@ package me.dusheiko.profile;
 
 import java.util.Locale;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -18,7 +21,10 @@ public class ProfileController {
 	}
 	
 	@RequestMapping(value = "/profile", method = RequestMethod.POST)
-	public String saveProfile(ProfileForm profileForm) {
+	public String saveProfile(@Valid ProfileForm profileForm, BindingResult bindingResult) {
+		if(bindingResult.hasErrors()) {
+			return "profile/profilePage";
+		}
 		System.out.println("save ok" + profileForm);
 		return "redirect:/profile";
 	}
--- src/main/java/me/dusheiko/profile/ProfileForm.java
+++ src/main/java/me/dusheiko/profile/ProfileForm.java
@@ -4,10 +4,20 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotEmpty;
+
 public class ProfileForm {
+	@Size(min = 2)
 	private String twitterHandle;
+	@Email
 	private String email;
+	@NotNull
 	private LocalDate birthDate;
+	@NotEmpty
 	private List<String> tastes = new ArrayList<>();
 	
 	public String getTwitterHandle() {
@@ -34,6 +44,4 @@ public class ProfileForm {
 	public void setTastes(List<String> tastes) {
 		this.tastes = tastes;
 	}
-	
-	
 }
