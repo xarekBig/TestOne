2f318ea8ad0bd25cb9c798ad7deed98ed5c5a6e6
dudi <dusheyko.dima@gmail.com>
2016-01-04 22:01:43 +0200

    Add profile page

---
+++ src/main/java/me/dusheiko/profile/ProfileController.java
@@ -0,0 +1,20 @@
+package me.dusheiko.profile;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class ProfileController {
+
+	@RequestMapping("/profile")
+	public String displayProfile(ProfileForm profileForm) {
+		return "profile/profilePage";
+	}
+	
+	@RequestMapping(value = "/profile", method = RequestMethod.POST)
+	public String saveProfile(ProfileForm profileForm) {
+		System.out.println("save ok" + profileForm);
+		return "redirect:/profile";
+	}
+}
