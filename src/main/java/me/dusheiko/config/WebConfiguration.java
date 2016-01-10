c522c9ff0111242a633b2a64ff6e8a9e80650484
dudi <dusheyko.dima@gmail.com>
2016-01-10 17:43:42 +0200

    add swagger tool

--- src/main/java/me/dusheiko/config/WebConfiguration.java
+++ src/main/java/me/dusheiko/config/WebConfiguration.java
@@ -4,7 +4,9 @@ import java.time.LocalDate;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
@@ -13,9 +15,16 @@ import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 import org.springframework.web.util.UrlPathHelper;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
 import me.dusheiko.date.USLocalDateFormater;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
+@EnableSwagger2
 public class WebConfiguration extends WebMvcConfigurerAdapter{
 	
 	@Override
@@ -45,5 +54,22 @@ public class WebConfiguration extends WebMvcConfigurerAdapter{
 		UrlPathHelper urlPathHelper = new UrlPathHelper();
 		urlPathHelper.setRemoveSemicolonContent(false);
 		configurer.setUrlPathHelper(urlPathHelper);
+		configurer.setUseRegisteredSuffixPatternMatch(true);
+	}
+	
+	@Bean
+	@Primary
+	public ObjectMapper objectMapper(Jackson2ObjectMapperBuilder builder) {
+		ObjectMapper objectMapper = builder.createXmlMapper(false).build();
+		objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+		return objectMapper;
+	}
+	
+	@Bean
+	public Docket userApi() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.paths(path -> path.startsWith("/api/"))
+				.build();
 	}
 }
