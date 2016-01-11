22211088140f8246c6fb56bac20af891d72274ed
sysop999 <sysop999@mail.ru>
2016-01-11 18:09:58 +0200

    add test profile

--- src/test/java/me/dusheiko/search/SearchControllerStubTest.java
+++ src/test/java/me/dusheiko/search/SearchControllerStubTest.java
@@ -9,6 +9,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
@@ -20,6 +21,7 @@ import me.dusheiko.MasteringSpringMvcApplication;
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = {MasteringSpringMvcApplication.class, StubTwitterSearchConfiguration.class})
 @WebAppConfiguration
+@ActiveProfiles("test")
 public class SearchControllerStubTest {
 	@Autowired
 	private WebApplicationContext wac;
--- src/test/java/me/dusheiko/search/StubTwitterSearchConfiguration.java
+++ src/test/java/me/dusheiko/search/StubTwitterSearchConfiguration.java
@@ -5,12 +5,14 @@ import java.util.Arrays;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 
 import me.dusheiko.search.LightTweet;
 import me.dusheiko.search.TwitterSearch;
 
 @Configuration
 public class StubTwitterSearchConfiguration {
+	@Profile("test")
 	@Primary
 	@Bean
 	public TwitterSearch twitterSearch() {
